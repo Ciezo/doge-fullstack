c953685951cdb4b9d36a280847f6996d9e9a241d
Cloyd Van <abcd123.letters@gmail.com>
2022-10-25 17:55:46 +0800

    Admin Model

---
+++ src/main/java/com/rijai/LocationApi/model/Admin.java
@@ -0,0 +1,81 @@
+package com.rijai.LocationApi.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String username;
+    private String password;
+
+    public Admin(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    /**
+     * Default constructor
+     */
+    public Admin() {}
+
+    /**
+     * Getters and setters
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.username);
+        hash = 79 * hash + Objects.hashCode(this.password);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Admin admin = (Admin) o;
+        return id == admin.id && username.equals(admin.username) && password.equals(admin.password);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
