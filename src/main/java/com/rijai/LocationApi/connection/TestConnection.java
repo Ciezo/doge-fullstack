173fc9b1cc044e7f6186997982b0600d021a2fca
Cloyd Van <abcd123.letters@gmail.com>
2022-10-26 19:10:35 +0800

    Removed entity package

--- src/main/java/com/rijai/LocationApi/connection/TestConnection.java
+++ src/main/java/com/rijai/LocationApi/connection/TestConnection.java
@@ -1,5 +1,6 @@
 package com.rijai.LocationApi.connection;
 
+
 public class TestConnection {
     public static void main(String[] args) {
         EstablishConnection connection = new EstablishConnection();
